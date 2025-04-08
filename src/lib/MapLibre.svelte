2625115adf2ee32c89ffbe163a9f1a9d393f4d7a
Gees Brouwer <geesbrouwer@icloud.com>
2025-04-08 16:40:39 +0200

    Made sheets on map clickable (+ a 'Sheet Controls' Component)

--- src/lib/MapLibre.svelte
+++ src/lib/MapLibre.svelte
@@ -1,15 +1,22 @@
 <link rel="stylesheet" href="https://use.typekit.net/dwr8fxs.css">
 <script lang="ts">
   import { onMount } from 'svelte';
+  import { tick } from 'svelte';
+  import feather from 'feather-icons';
   import maplibre from 'maplibre-gl';
   import 'maplibre-gl/dist/maplibre-gl.css';
   import { WarpedMapLayer } from '@allmaps/maplibre'
 
+  $: if (selectedSheet) {
+    tick().then(() => {
+      feather.replace();
+    });
+  }
+
   let map : maplibre.Map;
   let mapContainer: HTMLDivElement;
-  let selectedImage : ImageBitmap | null = $state(null);
   
-  let selectedEditions: Record<number, boolean> = { 1: true, 2: false, 5: true };
+  let selectedEditions: Record<number, boolean> = { 1: false, 2: false, 3: true, 4: false, 5: false };
   let warpedMapLayers: Record<number, WarpedMapLayer> = {
     1: new WarpedMapLayer("warped-map-1"),
     2: new WarpedMapLayer("warped-map-2"),
@@ -19,6 +26,9 @@
   };
 
   let showLabels : boolean = true;
+
+  let selectedSheet = null;
+  let selectedSheetFullScreen = false;
 
   async function fetchAndAddLayer(edition : number, layer : WarpedMapLayer) {
     const url = `https://raw.githubusercontent.com/bmmeijers/iiif-annotations/refs/heads/develop/series/waterstaatskaart/uu/editie_${edition}/latest.json`;
@@ -46,6 +56,8 @@
   }
 
   onMount(() => {
+    feather.replace();
+
     map = new maplibre.Map({
       container: mapContainer,
       style: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json',
@@ -62,8 +74,13 @@
         console.log(layer);
         if(!selectedEditions[+edition]) map.setLayoutProperty(layer.id, 'visibility', 'none');
       });
-      map.repaint = true;
+      // map.repaint = true;
+
+      map.setPaintProperty('water', 'fill-color', 'rgb(230,221,196)');
+      map.setPaintProperty('water_shadow', 'fill-color', 'rgb(230,221,196)');
     });
+
+    map.addControl(new maplibre.NavigationControl(), 'bottom-left');
 
     map.on('click', (e) => {
       const { lng, lat } = e.lngLat;
@@ -78,14 +95,10 @@
         const hitResults = layer.renderer.warpedMapList.rtree.searchFromPoint([lng, lat], true);
         if (hitResults.length > 0) {
           const hitImages = hitResults.map(id => layer.renderer?.warpedMapList.warpedMapsById.get(id));
+          selectedSheet = hitImages[0];
           console.log(`Topmost hit (Edition ${edition}):`, hitImages[0]);
-          
-          selectedImage = hitImages[0]?.cachedTilesForTexture[0].data;
-          setTimeout(() => {
-            const ctx = document.getElementById('sheetCanvas').getContext('2d');
-            ctx.drawImage(selectedImage, 0, 0)
-          }, 500)
 
+          showPolygonOnMap(map, hitImages[0]?.geoMask)
           return hitImages;
         }
       }
@@ -94,6 +107,50 @@
       return null;
     });
   });
+
+  function showPolygonOnMap(map, polygonGeoJson) {
+    const sourceId = 'dynamic-polygon';
+
+    if (map.getSource(sourceId)) {
+      map.getSource(sourceId).setData({
+        type: 'Feature',
+        geometry: polygonGeoJson
+       });
+    } else {
+      map.addSource(sourceId, {
+        type: 'geojson',
+        data: {
+          type: 'Feature',
+          geometry: polygonGeoJson
+        }
+      });
+
+      map.addLayer({
+        id: sourceId + 'line-2',
+        type: 'line',
+        source: sourceId,
+        paint: {
+          'line-color': 'rgba(255,255,255,.5)',
+          'line-width': 4
+        }
+      });
+
+      map.addLayer({
+        id: sourceId + '-line-1',
+        type: 'line',
+        source: sourceId,
+        paint: {
+          'line-color': 'rgb(236,98,183)',
+          'line-width': 2
+        }
+      });
+    }
+
+    // Bereken bounding box en zoom in
+    const bounds = new maplibre.LngLatBounds();
+    polygonGeoJson.coordinates[0].forEach(coord => bounds.extend(coord));
+    map.fitBounds(bounds, { padding: 100 });
+  }
 </script>
 
 <style>
@@ -111,15 +168,50 @@
     font-weight: 300;
     font-style: normal;
     position: absolute;
-    top: 60px;
+    top: 200px;
     left: 0;
     margin: 10px;
     padding: 15px;
+    z-index: -1;
+    transition: transform .3s;
+  }
+
+  .checkboxes .description {
+    position: relative;
+    left: -10px;
+    opacity: 0;
+    transition: all .3s;
+  }
+
+  .checkboxes:hover {
+    transform: translateX(10px);
+    background: #fff;
+    border-radius: 4px;
+    outline: 2px solid #00000022;
+  }
+
+  .checkboxes:hover .description {
+    opacity: 1;
+    left: 0;
   }
 
   .checkboxes label {
-    padding: 10px;
+    padding: 5px;
     display: block;
+  }
+
+  .checkboxes .icon {
+    position: relative;
+    left: -20px;
+    top: -3px;
+    font-size: 11px;
+    opacity: .2;
+    display: inline;
+  }
+
+  .checkboxes input[type="checkbox"]:checked + .icon {
+    color: white;
+    opacity: 1;
   }
 
   .checkboxes input[type="checkbox"] {
@@ -128,8 +220,8 @@
     appearance: none;
     width: 18px;
     height: 18px;
-    border: 1px solid rgb(200, 200, 255);
-    border-bottom: 2px solid rgb(200, 200, 255);
+    background: #fff;
+    border: 1px solid #00000022;
     border-radius: 4px;
     cursor: pointer;
     position: relative;
@@ -137,29 +229,69 @@
     left: -5px;
   }
   .checkboxes input[type="checkbox"]:checked {
-    background-color: rgba(58,86,171);
-    border: 1px solid rgba(58,86,171);
-    border-bottom: 2px solid rgba(58,86,171);
+    background-color: #000;
+    border: 1px solid #fff;
   }
 
-  .sheetOverlay {
+  .sheet-controls {
+    position: fixed;
+    bottom: 50px;
+    left: 50%;
+    margin-left: -200px;
+    width: 400px;
+    height: 90px;
+    background: #00000055;
+    outline: 2px solid #ffffff22;
+    border-radius: 4px;
+    /* padding: 10px; */
+    font-family: "ivypresto-display", serif;
+    font-weight: 400;
+    color: white;
+  }
+
+  .sheet-controls .image-btn {
+    height: 80px;
+    display: inline;
+    margin: 5px;
+    float: left;
+    cursor: pointer;
+  }
+
+  .sheet-controls .image-btn img {
+    height: 100%;
+  }
+
+  .sheet-controls .image-btn:hover {
+    filter: brightness(75%);
+  }
+  .sheet-controls .sheet-information {
+    padding: 10px;
+  }
+
+  .sheet-controls button {
+    padding: 4px;
+    cursor: pointer;
+  }
+
+  .sheet-controls button:hover {
+    background: #00000033;
+    border-radius: 4px;
+  }
+
+  .fullscreen-sheet {
+    height: 100vh;
+    width: 100vw;
     position: fixed;
     top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    background: rgba(0, 0, 0, 0.8);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    z-index: 1000;
+    text-align: center;
+    background: rgba(0,0,0,.25);
+    pointer-events: none;
   }
 
-  .sheetOverlay canvas {
-    position: relative;
-    max-width: 90vw;
-    max-height: 90vh;
-    overflow: hidden;
+  .fullscreen-sheet img {
+    width: 80vw;
+    margin: 150px auto;
+    box-shadow: 6px 6px 20px rgba(0,0,0,.5);
   }
 </style>
 
@@ -168,17 +300,38 @@
   {#each [1, 2, 3, 4, 5] as edition}
     <label>
       <input type="checkbox" checked={selectedEditions[edition] || false} on:change={() => toggleEdition(edition)} />
-      Editie {edition}
+      <i class="icon">{edition}</i>
+      <span class="description">Editie {edition}</span>
     </label>
   {/each}
   <label>
     <input type="checkbox" checked={showLabels} on:change={toggleLabels} />
-    Show labels
+    <i data-feather="tag" class="icon size-3" style="left: -22px; top: -2px;"></i>
+    <span class="description" style="position: relative; left: -10px;">Toon labels</span>
   </label>
 </div>
 
-{#if selectedImage}
-  <div class="sheetOverlay">
-    <canvas id="sheetCanvas" width="400" height="200"></canvas>
+{#if selectedSheet}
+<div class="sheet-controls">
+  <button class="image-btn" on:click={() => selectedSheetFullScreen = !selectedSheetFullScreen}>
+    <img src={selectedSheet.georeferencedMap.resource.id + '/0,0,' + selectedSheet.georeferencedMap.resource.width + ',' + selectedSheet.georeferencedMap.resource.height + '/128,/0/default.jpg'} alt="">
+  </button>
+  <!-- <img src={selectedSheet.georeferencedMap.resource.id} alt=""> -->
+  <div class="sheet-information">
+    <h2>Blad: {selectedSheet.imageId}</h2>
+    <br>
+    <div class="sheet-buttons">
+      <button><i data-feather="maximize-2" class="icon size-4"></i></button>
+      <button><i data-feather="columns" class="icon size-4"></i></button>
+      <button><i data-feather="external-link" class="icon size-4"></i></button>
+    </div>
   </div>
+
+</div>
+
+  {#if selectedSheetFullScreen}
+    <div class="fullscreen-sheet">
+      <img src={selectedSheet.georeferencedMap.resource.id + '/0,0,' + selectedSheet.georeferencedMap.resource.width + ',' + selectedSheet.georeferencedMap.resource.height + '/1024,/0/default.jpg'} alt="">
+    </div>
+  {/if}
 {/if}
